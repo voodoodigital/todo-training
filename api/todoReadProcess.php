fda2eafda84a9000d4895dd50659eeb4f9c50e2c
Shenal Rayantha <101634665+shenal-rd203@users.noreply.github.com>
2023-04-28 22:56:27 +0530

    delete comment

--- api/todoReadProcess.php
+++ api/todoReadProcess.php
@@ -1,23 +1,5 @@
 <?php
 
-// user access validation by check if the user is logged in from the request source. (if needed)
-
-// get the request data
-// only use one
-// most requests comes in JSON format
-// $request = $_GET["requestParameter"]; // if req on GET method
-// $request = $_POST["requestParameter"]; // if req on POST method
-// $requestObject = json_decode($request);
-
-// // import backend processes
-// require("../app/templateBackendProcess.php"); // file navigation is important 
-
-
-// do the APi process
-// ---> any process you need to do can be done here. also you can use predefined classes in other classes that you imported to this doc
-// make sure to create classes in 'app' folder for specific tasks.
-// only program bias process for the eact usecase and repeated processes can be create in the 'app' as classes and can  reuse in here
-
 $conn = new mysqli('localhost', 'root', '', 'todo_training_voodoo');
 
 if ($conn->connect_error) {
@@ -33,7 +15,7 @@ for ($i=0; $i < $result->num_rows; $i++) {
     $todoObject=new stdClass();
     $todoObject->todo=$data["todo"];
     $todoObject->time=$data["due_datetime"];
-    array_push($responseArray, $todoObject);    
+    array_push($responseArray, $todoObject);
 }
 
 $responseJsonText = json_encode($responseArray); // for arrays and objects
@@ -42,5 +24,3 @@ echo ($responseJsonText);
 $conn->close();
 
 ?>
-
-
