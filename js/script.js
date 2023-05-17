27054527b3167b7acaa1793dd2031f07526601ed
Shenal Rayantha <101634665+shenal-rd203@users.noreply.github.com>
2023-05-17 14:08:04 +0530

    ajax request to todoReadProcess

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 document.addEventListener("DOMContentLoaded", request);
 
 function request() {
-  // test
-  var responseArray = [
-    {
-      title: "title",
-      datetime: "123123",
-    },
-  ];
-  var container = document.getElementById("todoContainer");
-  for (let index = 0; index < responseArray.length; index++) {
-    var dataObject = responseArray[index];
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4) {
+      var response = request.responseText;
+      console.log(response);
+      var responseArray = JSON.parse(response);
+      var container = document.getElementById("todoContainer");
+      for (let index = 0; index < responseArray.length; index++) {
+        var dataObject = responseArray[index];
 
-    container.innerHTML += `
+        container.innerHTML += `
                 <div class=" col-6 shadow rounded-4 offset-3 p-3 my-2 bg-danger " style="max-height: 100px;">
                     <div class="h-100 text-white fw-bold justify-content-between d-flex">
-                      <div  class=" h-100 overflow-auto">${dataObject.title}</div>
-                      <div class="px-3">${dataObject.datetime}</div>
+                      <div  class=" h-100 overflow-auto">${dataObject.todo}</div>
+                      <div class="px-3">${dataObject.time}</div>
                     </div>
                 </div>
             `;
-  }
+      }
+    }
+  };
+  request.open(
+    "GET",
+    "http://localhost/abc/Voodoo/todo-training/api/todoReadProcess.php",
+    true
+  );
+  request.send();
 }
