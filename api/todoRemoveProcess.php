da16c659a21980566a4df1c3a0d8b93cb72212ae
Shenal Rayantha <101634665+shenal-rd203@users.noreply.github.com>
2023-05-23 22:26:15 +0530

    created remove process file

--- api/todoRemoveProcess.php
+++ api/todoRemoveProcess.php
@@ -1,23 +1,23 @@
 <?php
 
-// user access validation by check if the user is logged in from the request source. (if needed)
 
-// get the request data
-// only use one
-// most requests comes in JSON format
-$request = $_GET["requestParameter"]; // if req on GET method
-// $request = $_POST["requestParameter"]; // if req on POST method
+$request = $_GET["todoRemoveData"]; // if req on GET method
 $requestObject = json_decode($request);
 
 // import backend processes
-require("../app/templateBackendProcess.php"); // file navigation is important 
+require("../app/dbQuery.php"); // file navigation is important 
 
 
 // do the APi process
 // ---> any process you need to do can be done here. also you can use predefined classes in other classes that you imported to this doc
 // make sure to create classes in 'app' folder for specific tasks.
 // only program bias process for the eact usecase and repeated processes can be create in the 'app' as classes and can  reuse in here
+$database = new DB();
+$query = "DELETE FROM `todo` WHERE `status_id`=?";
+$stmt1 = $database->prepare($query, "ss", array($todo, $datetime));
 
+$responseObject->status = "success";
+ErrorSender::sendError($responseObject);
 
 // send the response
 $responseObject = new stdClass(); // if response in object format
